e23af1e64a41fa4a9a5889147554d058abe9eeea
guilbertf <guilbertf@a11p7.fil.univ-lille1.fr>
2015-10-09 16:29:53 +0200

    fais basket

--- Glouglou/src/BasketPool.java
+++ Glouglou/src/BasketPool.java
@@ -1,4 +1,14 @@
 
-public class BasketPool {
+public class BasketPool extends ResourcePool<Basket>{
 
+	public BasketPool(int n){
+		super(n);
+	}
+
+	@Override
+	protected Basket createResource() {
+		// TODO Auto-generated method stub
+		return newBasket();
+	}
+	
 }
--- Glouglou/src/ResourcePool.java
+++ Glouglou/src/ResourcePool.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
 
-public class ResourcePool <R extends Resource> {
 
+public abstract class ResourcePool <R extends Resource> {
+
+	private ArrayList<R> poolFree;
+	private ArrayList<R> poolUsed;
+	
+	public ResourcePool(int n){
+		this.poolFree= new ArrayList<R>(n);
+		this.poolUsed = new ArrayList<R>(n);
+		for(int i=0;i<n;i++){
+			poolFree.add(createResource());
+		}
+	}
+	
+	public R provideResource(){
+		if(hasAvailableResource()){
+			R r = poolFree.get(0);
+			poolFree.remove(0);
+			poolUsed.remove(0);
+		}
+		return null;
+	}
+	
+	private boolean hasAvailableResource() {
+		// TODO Auto-generated method stub
+		if(this.poolFree.get(0)== null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	public void freeResource(R r){
+		int indexResourceUsed=this.poolUsed.indexOf(r);
+		if(indexResourceUsed!=-1){
+			this.poolUsed.remove(indexResourceUsed);
+			this.poolFree.add(r);
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+	
+	protected abstract R createResource();
+	
 }

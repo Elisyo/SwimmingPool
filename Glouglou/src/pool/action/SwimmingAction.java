35d626a6f0707a2d2fa6d286e36d856b0f2a1688
breuzon <breuzon@a11p7.fil.univ-lille1.fr>
2015-10-16 16:37:04 +0200

    implementation de SwimmingAction Obsolète

--- Glouglou/src/pool/action/SwimmingAction.java
+++ Glouglou/src/pool/action/SwimmingAction.java
@@ -1,5 +1,34 @@
 package pool.action;
 
-public class SwimmingAction {
-
+public class SwimmingAction extends ForeseeableAction{
+	protected int totalTime;
+	protected int timeRemaining;
+	
+	public SwimmingAction(int totalTime){
+		this.totalTime = totalTime;
+		timeRemaining = totalTime;
+	}
+	
+	@Override
+	public boolean isReady(){
+		return totalTime==timeRemaining;
+	}
+	
+	@Override
+	public boolean isFinished(){
+		return timeRemaining==0;
+	}
+	
+	@Override
+	public boolean isInProgress(){
+		return timeRemaining < totalTime;
+	}
+	
+	@Override
+	public void reallyDoStep(){
+		if(!isFinished())
+			timeRemaining--;
+	}
 }
+
+//need to do in foreseebleAction

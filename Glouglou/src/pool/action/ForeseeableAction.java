6d8733d6929d22aa36a3050226ca78767d3fb9c7
breuzon <breuzon@a11p7.fil.univ-lille1.fr>
2015-10-16 16:50:43 +0200

    va-t-on garder IsInitialized dans Action ?

--- Glouglou/src/pool/action/ForeseeableAction.java
+++ Glouglou/src/pool/action/ForeseeableAction.java
@@ -1,29 +1,35 @@
 package pool.action;
 
 public class ForeseeableAction extends Action{
-
+	protected String name;
+	protected int totalTime;
+	protected int timeRemaining;
+	
+	public ForeseeableAction(String name){
+		this.name = name;
+	}
+	
 	@Override
-	public boolean isReady() {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isReady(){
+		return totalTime==timeRemaining;
+	}
+	
+	@Override
+	public boolean isFinished(){
+		return timeRemaining==0;
+	}
+	
+	
+	@Override
+	public void reallyDoStep(){
+		if(!isFinished())
+			timeRemaining--;
 	}
 
 	@Override
 	public boolean isInitialized() {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	@Override
-	public boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void reallyDoStep() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- Glouglou/src/pool/action/SwimmingAction.java
+++ Glouglou/src/pool/action/SwimmingAction.java
@@ -1,34 +1,14 @@
 package pool.action;
 
 public class SwimmingAction extends ForeseeableAction{
-	protected int totalTime;
-	protected int timeRemaining;
-	
-	public SwimmingAction(int totalTime){
-		this.totalTime = totalTime;
-		timeRemaining = totalTime;
+
+	public SwimmingAction(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
 	}
 	
-	@Override
-	public boolean isReady(){
-		return totalTime==timeRemaining;
-	}
 	
-	@Override
-	public boolean isFinished(){
-		return timeRemaining==0;
-	}
 	
-	@Override
-	public boolean isInProgress(){
-		return timeRemaining < totalTime;
-	}
-	
-	@Override
-	public void reallyDoStep(){
-		if(!isFinished())
-			timeRemaining--;
-	}
 }
 
 //need to do in foreseebleAction

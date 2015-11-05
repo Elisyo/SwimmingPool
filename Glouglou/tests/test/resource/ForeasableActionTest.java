061493d01c96bc3699f5dd14b3e64c3806abfd16
Elymak <camillebreuzon@orange.fr>
2015-11-05 22:30:57 +0100

    ForeasableActionTest ok

---
+++ Glouglou/tests/test/resource/ForeasableActionTest.java
@@ -0,0 +1,42 @@
+package test.resource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pool.action.Action;
+import pool.action.ForeseeableAction;
+import pool.exception.ActionFinishedException;
+import test.action.ActionTest;
+
+public class ForeasableActionTest extends ActionTest{
+
+	@Override
+	protected Action createAction() {
+		// TODO Auto-generated method stub
+		return new ForeseeableAction(2);
+	}
+	
+	@Test
+	public void foreasableTest() throws ActionFinishedException{
+		Action action = new ForeseeableAction(2);
+		
+		assertTrue(action.isReady());
+		assertFalse(action.isFinished());
+		assertTrue(action.isInitialized());
+		
+		action.doStep();
+		
+		assertFalse(action.isReady());
+		assertFalse(action.isFinished());
+		assertFalse(action.isInitialized());
+		
+		action.doStep();
+		
+		assertTrue(action.isFinished());
+		assertFalse(action.isReady());
+		assertFalse(action.isInitialized());
+	}
+
+
+}
